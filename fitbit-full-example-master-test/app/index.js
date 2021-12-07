6da6fa5227002626b44f25877f6a019f1c642393
Senne Brouwers <sennebrouwers@MacBook-Pro-van-Senne.local>
2021-12-07 13:55:36 +0100

    initial commit

--- fitbit-full-example-master-test/app/index.js
+++
@@ -1,9 +0,0 @@
-import { init as initState } from './state';
-import { init as initCommands } from './commands';
-import { init as initNavigation, switchPage } from './navigation';
-import router from './router';
-
-initState();
-initCommands();
-initNavigation(router);
-switchPage('index');
